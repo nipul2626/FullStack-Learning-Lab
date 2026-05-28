b0c2391a71a17bc29805c5bd4dfd24d8cc2902c6
nipul2626 <nipulrathod34@gmail.com>
2026-05-28 16:21:10 +0530

    Initial project setup

---
+++ src/main/java/com/example/FullStackLearningLab/FullStackLearningLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.FullStackLearningLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackLearningLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackLearningLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FullStackLearningLab/FullStackLearningLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FullStackLearningLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackLearningLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
