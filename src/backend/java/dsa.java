a6043bb38357dedd298f4c34fd2de6a02f894bb7
nipul2626 <nipulrathod34@gmail.com>
2026-05-28 19:23:08 +0530

    array print elements

---
+++ src/backend/java/dsa.java
@@ -0,0 +1,9 @@
+public class dsa{
+    public static void main(String[] args) {
+        int arr[] = {12,13,15,17,1,6,2,5};
+
+        for(int i = 0; i < arr.length; i++){
+            System.err.println(i);
+        }
+    }
+}
